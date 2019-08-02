54f7c2aa8f0e1c243c27cfaa4bfa05fdedc056d0
ArtemGord1728 <artemgord00@gmail.com>
2019-08-02 03:33:50 +0300

    added new directories with implementations responce from bot

---
+++ src/main/java/com/cloud/core/BotResponse.java
@@ -0,0 +1,8 @@
+package com.cloud.core;
+
+public class BotResponse {
+	
+	public final static String START_RESPONCE = "Some start";
+	public final static String ACTIONS_RESPONCE = "What action will you choose to process the data?";
+	public final static String HELP_RESPONCE = "Some help";
+}
