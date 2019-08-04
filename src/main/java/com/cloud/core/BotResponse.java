297ad37f7cb7a6a6d984523dc980a8909c8036ba
ArtemGord1728 <artemgord00@gmail.com>
2019-08-04 05:34:43 +0300

    added new constant

--- src/main/java/com/cloud/core/BotResponse.java
+++ src/main/java/com/cloud/core/BotResponse.java
@@ -4,5 +4,6 @@ public class BotResponse {
 	
 	public final static String START_RESPONCE = "Some start";
 	public final static String ACTIONS_RESPONCE = "What action will you choose to process the data?";
+	public final static String ARCHIVE_RESPONCE = "Select archive extencion, please:";
 	public final static String HELP_RESPONCE = "Some help";
 }
